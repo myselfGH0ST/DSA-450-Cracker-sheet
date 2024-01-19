2ce0a9b1b749c4ec54077348cad4f7194aaa58d8
G.H.O.S.T <kaushikofficial.agt@gmail.com>
2024-01-19 16:30:55 +0530

    array - 5

--- Array/array_5.cpp
+++ Array/array_5.cpp
@@ -16,27 +16,54 @@ using namespace std;
 
 int main()
 {
+    unordered_map<int,int> mp;
     int a[]={1,2,4,6,8};
     int b[]={2,3,4};
     int c=0;
+    int n=sizeof(a)/sizeof(a[0]);
+    int m=sizeof(b)/sizeof(b[0]);
 
-    int n1=sizeof(a)/sizeof(a[0]);
-    int n2=sizeof(b)/sizeof(b[0]);
-
-    for(int i=0;i<n1;i++)
+    
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n2;j++)
-        {
-            if(b[j]==a[i])
-            {
-                c++;
-                break;
-            }
-        }
+        mp[a[i]]=c;
+        c++;
+    }
+    for(int j=0;j<m;j++)
+    {
+        mp[b[j]]=c;
+        c++;
     }
 
-    cout<<"common elements "<<c<<endl;
-    cout<<"union elements "<<((n1-c)+c+(n2-c))<<endl;
+    cout<<"size of union is "<<mp.size()<<endl;
     
+
     return 0;
 }
+
+// int main()
+// {
+//     int a[]={1,2,4,6,8};
+//     int b[]={2,3,4};
+//     int c=0;
+
+//     int n1=sizeof(a)/sizeof(a[0]);
+//     int n2=sizeof(b)/sizeof(b[0]);
+
+//     for(int i=0;i<n1;i++)
+//     {
+//         for(int j=0;j<n2;j++)
+//         {
+//             if(b[j]==a[i])
+//             {
+//                 c++;
+//                 break;
+//             }
+//         }
+//     }
+
+//     cout<<"common elements "<<c<<endl;
+//     cout<<"union elements "<<((n1-c)+c+(n2-c))<<endl;
+    
+//     return 0;
+// }
